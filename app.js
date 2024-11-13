9656eec4bb6fac56464844c8f33846f0b94a94fa
Satya Kalyan <satyakalyan5@gmail.com>
2024-11-12 21:47:17 -0700

    Modified the app.js

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 require("dotenv").config();
 require("express-async-errors");
 
-//extra security packages
+// extra security packages
 const helmet = require("helmet");
 const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 
+// Swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
 const express = require("express");
 const app = express();
 
-//connectDB
 const connectDB = require("./db/connect");
 const authenticateUser = require("./middleware/authentication");
-
-//routers
+// routers
 const authRouter = require("./routes/auth");
 const jobsRouter = require("./routes/jobs");
-
 // error handler
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
@@ -25,15 +27,19 @@ const errorHandlerMiddleware = require("./middleware/error-handler");
 app.set("trust proxy", 1);
 app.use(
 	rateLimiter({
-		windowMs: 15 * 60 * 1000, //15 minutes
-		max: 100, //limit each IP to 100 requests per windowMs
+		windowMs: 15 * 60 * 1000, // 15 minutes
+		max: 100, // limit each IP to 100 requests per windowMs
 	})
 );
 app.use(express.json());
-// extra packages
 app.use(helmet());
 app.use(cors());
 app.use(xss());
+
+app.get("/", (req, res) => {
+	res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+});
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
 app.use("/api/v1/auth", authRouter);
@@ -48,7 +54,7 @@ const start = async () => {
 	try {
 		await connectDB(process.env.MONGO_URI);
 		app.listen(port, () =>
-			console.log(`Server is listening on the port ${port}...`)
+			console.log(`Server is listening on port ${port}...`)
 		);
 	} catch (error) {
 		console.log(error);
